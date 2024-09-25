f531f62b768f169d067bf765dcf617467309b38a
JeongWoo <jwkang827@gmail.com>
2024-09-26 04:26:15 +0900

    비밀정보 보호

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: ''//헤로쿠 몽고db 연결
+}
--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { User } = require('./models/User')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://jwkang827:Dil8MyZ5nAEBMN2m@boilerplate.tpyn8.mongodb.net/')
-.then(() => console.log('MongDB Connected!')).catch(err => console.log(err))
+    .then(() => console.log('MongDB Connected!')).catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/register', (req, res) => {
 
-// 
+    const user = new User(req.body)
+
+    user.save()
+        .then(() => res.status(200).json({ success: true }))
+        .catch((err) => res.json({ success: false, err }))
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
